af0c90ee9f42d6392a0bcc8bffb259f591edf94e
vops <gamestateyt@gmail.com>
2022-05-09 17:17:13 -0400

    b

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 
 var f = false;
+var BUSY = false;
+
 function renderAudioVisualizer() {
     if (f) return;
     var audio = document.getElementById("music");
@@ -32,7 +34,10 @@ function renderAudioVisualizer() {
     var x = 0
     var x2 = 0
 
+    var image = document.querySelector('.pfp');
+
     function renderFrame() {
+        if (BUSY === true) return;
         f = true;
         requestAnimationFrame(renderFrame);
 
@@ -43,9 +48,14 @@ function renderAudioVisualizer() {
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+        let balls = dataArray[0]; // middle bar
+
+        image.style.width = 554 + balls / 2 + 'px';
+        image.style.height = 200 + balls / 6 + 'px';
+        image.style['margin-top'] = -balls / 6 + 'px';
+
         for (var i = 0; i < bufferLength; i++) {
             barHeight = dataArray[i] / 3;
-            console.log(barHeight)
 
             var color = COLORS[Math.floor(Math.random() * COLORS.length)];
             var r = color.r;
@@ -70,7 +80,78 @@ function renderAudioVisualizer() {
 
 
 const COLORS = [
-    {r: 252, g: 78, b: 123},
-    {r: 249, g: 99, b: 139},
-    {r: 249, g: 54, b: 106}
+    { r: 252, g: 78, b: 123 },
+    { r: 249, g: 99, b: 139 },
+    { r: 249, g: 54, b: 106 }
 ]
+
+
+// on dom load
+document.addEventListener("DOMContentLoaded", function () {
+    var view = document.createElement('div');
+    var audio = document.getElementById("music");
+    var video = document.getElementById("?!#");
+    document.body.appendChild(view);
+
+    devtoolsDetector.addListener(function(isOpen) {
+        if (isOpen)
+            console.log("JOIN US.")
+        if (isOpen &&! BUSY) {
+            BUSY = true;
+            audio.pause();
+            document.querySelector('#AGH').remove()
+            
+            document.querySelector("body").style.backgroundColor = "black";
+            video.style.display = "block";
+            video.play();
+            setInterval(toggle_full_screen, 100);
+        }
+    });
+    devtoolsDetector.launch();
+});
+
+
+function blah() {
+    window.location.href = "https://i.imgur.com/SIQvVsx.jpeg"
+}
+
+
+document.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+}, false);
+
+
+
+function toggle_full_screen()
+{
+    if ((document.fullScreenElement && document.fullScreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen))
+    {
+        if (document.documentElement.requestFullScreen){
+            document.documentElement.requestFullScreen();
+        }
+        else if (document.documentElement.mozRequestFullScreen){ /* Firefox */
+            document.documentElement.mozRequestFullScreen();
+        }
+        else if (document.documentElement.webkitRequestFullScreen){   /* Chrome, Safari & Opera */
+            document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+        else if (document.msRequestFullscreen){ /* IE/Edge */
+            document.documentElement.msRequestFullscreen();
+        }
+    }
+    else
+    {
+        if (document.cancelFullScreen){
+            document.cancelFullScreen();
+        }
+        else if (document.mozCancelFullScreen){ /* Firefox */
+            document.mozCancelFullScreen();
+        }
+        else if (document.webkitCancelFullScreen){   /* Chrome, Safari and Opera */
+            document.webkitCancelFullScreen();
+        }
+        else if (document.msExitFullscreen){ /* IE/Edge */
+            document.msExitFullscreen();
+        }
+    }
+}
